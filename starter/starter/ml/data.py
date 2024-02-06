d2db417db952f3e4aa1405918ac2012d140de3c1
Aykhan Amrahov <aykhanam@azrc-admins-MacBook-Pro-10.local>
2024-02-06 18:57:41 +0400

    model ready

--- starter/starter/ml/data.py
+++ starter/starter/ml/data.py
@@ -1,9 +1,22 @@
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+from .model import compute_model_metrics
+
+categorical_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
 
 
 def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
+        X, categorical_features=[], label=None, training=True, encoder=None, lb=None
 ):
     """ Process the data used in the machine learning pipeline.
 
@@ -54,7 +67,7 @@ def process_data(
     X_continuous = X.drop(*[categorical_features], axis=1)
 
     if training is True:
-        encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+        encoder = OneHotEncoder(handle_unknown="ignore",sparse_output=False)
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
         y = lb.fit_transform(y.values).ravel()
@@ -65,6 +78,44 @@ def process_data(
         # Catch the case where y is None because we're doing inference.
         except AttributeError:
             pass
-
     X = np.concatenate([X_continuous, X_categorical], axis=1)
     return X, y, encoder, lb
+
+
+def load_data(path):
+    """
+        Function to load data from a file given
+        Parameters:
+            path: Path to the data file: str
+        Returns:
+            df: Input data frame for model
+    """
+    # Load data
+    df = pd.read_csv(path)
+    return df
+
+
+def slide_performance(model, data, encoder, lb, categorical_features=categorical_features):
+    """
+        Function to calculate the performance of the model on slices of the data
+        Parameters:
+            model: ML model
+            data: data need calulating the performance
+            encoder: sklearn.preprocessing._encoders.OneHotEncoder
+            lb: sklearn.preprocessing._label.LabelBinarizer
+            categorical_features: the categorical features list
+        Returns:
+            file: create the file containing the results of the silde performance
+    """
+    with open('log_slice_performance.txt', 'w') as f:
+        for categorical in categorical_features:
+            for value in data[categorical].unique():
+                data_preformance = data[data[categorical] == value]
+                X, y, _, _ = process_data(data_preformance, categorical_features=categorical_features, label="salary",
+                                          training=False, encoder=encoder, lb=lb)
+                y_pred = model.predict(X)
+                precision, recall, fbeta = compute_model_metrics(y, y_pred)
+                line = "Feature {value} of the Categorical {cat}: Precision:{precision} | Recall:{recall} | Fbeta:{fbeta}\n".format(
+                    value=value, cat=categorical, precision=precision, recall=recall, fbeta=fbeta)
+                f.write(line)
+    f.close()
